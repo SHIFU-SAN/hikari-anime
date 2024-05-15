5fcf7d96b24bfa222203dd4a2d1178844afed67e
ShiFu <phunguyenchi2001@gmail.com>
2024-05-15 21:46:14 +0700

    first commit

---
+++ front-end/app/layout.jsx
@@ -0,0 +1,8 @@
+import "../assets/css/globals.css"
+export default function RootLayout({children}) {
+    return (
+        <html lang="en">
+        <body>{children}</body>
+        </html>
+    )
+}
