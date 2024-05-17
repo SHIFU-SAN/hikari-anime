859f41c89c87add317fc35509dff2f761e9d5839
ShiFu <phunguyenchi2001@gmail.com>
2024-05-17 22:01:56 +0700

    Reset UI/UX

--- front-end/app/page.jsx
+++ front-end/app/page.jsx
@@ -1,5 +1,7 @@
+
+
 export default function Page() {
-    return <h1 className="text-3xl font-bold underline">
-        Hello world!
-    </h1>
+    return <>
+        <h1>Hello World</h1>
+    </>
 }
